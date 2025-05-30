218b543b75c0fb937fa0f2ee460b1396f37c3959
Dan Morgan <dmorgan@shookconstruction.com>
2025-05-30 17:44:16 +0000

    custom_schema_name fix

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,7 +1,9 @@
 {% macro generate_schema_name(custom_schema_name, node) %}
-  {% if env_var('DBT_IS_PROD', 'false') == 'true' %}
-    {{ customer_schema_name }}
+  {% set is_prod = env_var('DBT_ENV_NAME', 'dev') | lower == 'prod' %}
+  
+  {% if is_prod %}
+    {{ custom_schema_name }}
   {% else %}
-    {{ target.schema }}_{{ custom_schema_name if custom_schema_name else node.name }}
+    {{ target.schema }}_{{ custom_schema_name }}
   {% endif %}
 {% endmacro %}
