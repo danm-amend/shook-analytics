8e8e630c7a528c21335145587a26e5b80b912806
Dan Morgan <dmorgan@shookconstruction.com>
2025-06-11 13:00:57 +0000

    Reverted job_bounds CTE in job_cost_agg query to ensure grouping by first day of month only based on "Mth" in actual_cost

--- models/shook_analytics/profitability/job_cost_agg.sql
+++ models/shook_analytics/profitability/job_cost_agg.sql
@@ -1,20 +1,31 @@
 WITH 
+    -- job_bounds AS (
+    --   SELECT 
+    --     Jcco
+    --     ,Job
+    --     ,COALESCE(cm.START_DATE, cm.START_MONTH, ac.min_month) as min_month
+    --     ,COALESCE(cm.ACTUAL_CLOSE_DATE, cm.MONTH_CLOSED, ac.max_month) as max_month
+    --   from {{ ref('contract_master') }} as cm
+    --   left join 
+    --   (
+    --     select Jcco, Job
+    --     ,MIN(DATE_TRUNC('MONTH', CAST(Mth AS DATE))) AS min_month
+    --     ,MAX(DATE_TRUNC('MONTH', CAST(Mth AS DATE))) AS max_month
+    --     FROM {{ ref('actual_cost') }}
+    --     GROUP BY jcco, job
+    --   ) as ac 
+    --   using(jcco, job)
+    -- ),
     job_bounds AS (
-      SELECT 
-        Jcco
-        ,Job
-        ,COALESCE(cm.START_DATE, cm.START_MONTH, ac.min_month) as min_month
-        ,COALESCE(cm.ACTUAL_CLOSE_DATE, cm.MONTH_CLOSED, ac.max_month) as max_month
-      from {{ ref('contract_master') }} as cm
-      left join 
-      (
-        select Jcco, Job
-        ,MIN(DATE_TRUNC('MONTH', CAST(Mth AS DATE))) AS min_month
-        ,MAX(DATE_TRUNC('MONTH', CAST(Mth AS DATE))) AS max_month
+        select 
+            JCCo
+            , Job
+            ,MIN(DATE_TRUNC('MONTH', CAST(Mth AS DATE))) AS min_month
+            ,MAX(DATE_TRUNC('MONTH', CAST(Mth AS DATE))) AS max_month
         FROM {{ ref('actual_cost') }}
-        GROUP BY jcco, job
-      ) as ac 
-      using(jcco, job)
+        GROUP BY 
+            JCCo
+            , Job
     ),
     current_job_bounds AS (
         SELECT *
