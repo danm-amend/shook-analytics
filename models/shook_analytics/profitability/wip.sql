ca54eed2a4e6a1dcb77bd95d94eaf3236c4ddff7
Dan Morgan <dmorgan@shookconstruction.com>
2025-08-08 19:47:12 +0000

    Update WIP logic with overrides table data

--- models/shook_analytics/profitability/wip.sql
+++ models/shook_analytics/profitability/wip.sql
@@ -1,5 +1,5 @@
 WITH
-    costs_by_job_month as (
+    costs_history as (
         SELECT 
             "JCCO" AS jcco
             ,"JOB" AS job
@@ -26,128 +26,126 @@ WITH
             ,"CONTRACT"
             ,DATEFROMPARTS(YEAR(CAST("CHANGE_DATE" AS DATE)), MONTH(CAST("CHANGE_DATE" AS DATE)), 1)
     ),
-    base_wip_by_month AS (
+    overrides_by_month AS (
+        SELECT
+            "JCCO" AS jcco
+            ,"CONTRACT" AS contract
+            ,CAST(MTH AS DATE) AS mth
+            ,NULLIF(SUM(CONTRACTAMT),0) AS contract_amt
+            ,NULLIF(SUM(BILLEDAMT), 0) AS billed_amt
+            ,NULLIF(SUM(PROJREVENUE),0) AS proj_revenue
+            ,NULLIF(SUM(PROJCOST),0) AS proj_cost
+            ,NULLIF(SUM(PROJOVERCOST),0) AS proj_over_cost
+            ,NULLIF(SUM(ACTUALCOST),0) AS actual_cost
+            ,NULLIF(SUM(CURRESTCOST),0) AS estimated_cost
+        FROM 
+            shookdw.viewpoint.brvJCCostRevenueOverride
+        GROUP BY 
+            "JCCO"
+            ,"CONTRACT"
+            ,CAST(MTH AS DATE)
+    ),
+    wip_history_0 AS (
         SELECT 
             costs.jcco
             ,costs.job
             ,costs.mth
             ,costs.cumulative_actual_cost
             ,costs.cumulative_projected_cost
-            --,contracts.contract_amt_change
+            ,contracts.contract_amt_change
             ,SUM(contracts.contract_amt_change) OVER (PARTITION BY costs.jcco, costs.job ORDER BY costs.mth) AS current_contract_amt
+            -- "or" prefix = "override"
+            -- "cor" prefix = "cumulative override"
+            ,overrides.contract_amt AS or_contract_amt
+            ,SUM(overrides.contract_amt) OVER (PARTITION BY costs.jcco, costs.job ORDER BY costs.mth) AS cor_contract_amt
+            ,overrides.billed_amt AS or_billed_amt
+            ,SUM(overrides.billed_amt) OVER (PARTITION BY costs.jcco, costs.job ORDER BY costs.mth) AS cor_billed_amt
+            ,overrides.proj_revenue AS or_proj_revenue
+            ,SUM(overrides.proj_revenue) OVER (PARTITION BY costs.jcco, costs.job ORDER BY costs.mth) AS cor_proj_revenue
+            ,overrides.proj_cost AS or_proj_cost
+            ,SUM(overrides.proj_cost) OVER (PARTITION BY costs.jcco, costs.job ORDER BY costs.mth) AS cor_proj_cost
+            ,overrides.proj_over_cost AS or_proj_over_cost
+            ,SUM(overrides.proj_over_cost) OVER (PARTITION BY costs.jcco, costs.job ORDER BY costs.mth) AS cor_proj_over_cost
+            ,overrides.actual_cost AS or_actual_cost
+            ,SUM(overrides.actual_cost) OVER (PARTITION BY costs.jcco, costs.job ORDER BY costs.mth) AS cor_actual_cost
+            ,overrides.estimated_cost AS or_estimated_cost
+            ,SUM(overrides.estimated_cost) OVER (PARTITION BY costs.jcco, costs.job ORDER BY costs.mth) AS cor_estimated_cost
         FROM 
-            costs_by_job_month AS costs
+            costs_history AS costs
             LEFT JOIN contracts_by_month AS contracts
                     ON costs.jcco = contracts.jcco
                     AND costs.job = contracts.contract
                     AND costs.mth = contracts.mth
+            LEFT JOIN overrides_by_month AS overrides
+                    ON costs.jcco = overrides.jcco
+                    AND costs.job = overrides.contract
+                    AND costs.mth = overrides.mth
     ),
-    contra_jobs AS (
-        SELECT DISTINCT
+    wip_history_1 AS (
+        SELECT
             jcco
             ,job
             ,mth
-            ,LEFT(job, 7) AS non_contra_job
+            ,ROUND(COALESCE(or_proj_revenue, current_contract_amt), 0) AS TJ_Contract_Amount
+            ,ROUND(COALESCE(or_proj_over_cost, cumulative_projected_cost), 0) AS TJ_Cost
+            ,ROUND(COALESCE(cor_actual_cost, cumulative_actual_cost), 0) AS JTD_Cost
+            --,COALESCE(cor_billed_amt, ) AS JTD_Billings --TODO: join billings in
         FROM 
-            base_wip_by_month
-        WHERE
-            job LIKE '%.001%'
+            wip_history_0
     ),
-    /*
-
-    Once completed vs in progress logic is determined, contra jobs that are completed will need adjusted to match the WIP file rather than showing contract amount of 0
-
-    Completed Contra:
-    Contract Amt and Projected Cost = Cumulative Actual Cost
-
-    */
-    contra_jobs_adjusted AS (
-        SELECT 
-            -- contra.jcco AS contra_jcco
-            -- ,contra.job AS contra_job
-            -- ,contra.mth AS contra_mth
-            -- ,contra.non_contra_job
-            -- ,wip.jcco AS wip_jcco
-            -- ,wip.job AS wip_job
-            -- ,wip.mth AS wip_mth
-            -- ,wip.current_contract_amt
-            contra.jcco
-            ,contra.job
-            ,contra.mth
-            ,wip.current_contract_amt * -1 AS contra_current_contract_amt
-            ,wip.current_contract_amt * -1 AS contra_cumulative_projected_cost
-        FROM
-            contra_jobs AS contra
-                LEFT JOIN base_wip_by_month AS wip
-                    ON contra.jcco = wip.jcco AND contra.non_contra_job = wip.job AND contra.mth = wip.mth
-    ), -- SELECT * FROM contra_jobs_contract_amt WHERE job = '123043.001' ORDER BY mth
-    wip_contra_adjusted AS (
-        SELECT
-            wip.jcco
-            ,wip.job
-            ,wip.mth
-            ,wip.cumulative_actual_cost
-            ,COALESCE(contra.contra_cumulative_projected_cost, wip.cumulative_projected_cost) AS cumulative_projected_cost
-            ,COALESCE(contra.contra_current_contract_amt, wip.current_contract_amt) AS current_contract_amt
-        FROM
-            base_wip_by_month AS wip
-                LEFT JOIN contra_jobs_adjusted AS contra
-                    ON wip.jcco = contra.jcco AND wip.job = contra.job AND wip.mth = contra.mth
-    ),
-    wip_calcs_1 AS (
+    wip_history_2 AS (
         SELECT
             *
-            ,CASE
-                WHEN cumulative_projected_cost = 0 THEN 0
-                ELSE cumulative_actual_cost / cumulative_projected_cost
-            END AS percent_complete
+            ,TJ_Contract_Amount - TJ_Cost AS TJ_Margin
+            ,CASE 
+                WHEN TJ_Cost = 0 THEN 0 
+                ELSE JTD_Cost / TJ_Cost 
+            END AS JTD_Percent_Complete
         FROM
-            wip_contra_adjusted
+            wip_history_1
     ),
-    wip_calcs_2 AS (
-        SELECT 
-            *
-            ,current_contract_amt - cumulative_projected_cost AS projected_margin_dollars
-            ,CASE
-                WHEN current_contract_amt - cumulative_projected_cost < 0 THEN current_contract_amt - cumulative_projected_cost
-                ELSE (current_contract_amt - cumulative_projected_cost) * percent_complete
-            END AS cumulative_margin_dollars
-        FROM 
-            wip_calcs_1
-    ),
-    wip_calcs_3 AS (
-        SELECT 
-            *
-            ,CASE
-                WHEN current_contract_amt = 0 THEN 0
-                ELSE projected_margin_dollars / current_contract_amt
-            END AS projected_margin_pct
-            ,cumulative_actual_cost + cumulative_margin_dollars AS cumulative_revenue
-            ,current_contract_amt - (cumulative_actual_cost + cumulative_margin_dollars) AS backlog_revenue
-            ,projected_margin_dollars - cumulative_margin_dollars AS backlog_margin
-        FROM 
-            wip_calcs_2
-    ),
-    wip_calcs_4 AS (
+    wip_history_3 AS (
         SELECT
-            *,
-            cumulative_revenue - LAG(cumulative_revenue) OVER (PARTITION BY jcco, job ORDER BY mth) AS this_month_revenue
-            ,cumulative_margin_dollars - LAG(cumulative_margin_dollars) OVER (PARTITION BY jcco, job ORDER BY mth) AS this_month_margin
-            ,MAX(mth) OVER (PARTITION BY jcco, job) AS max_month
-        FROM 
-            wip_calcs_3
+            *
+            ,CASE 
+                WHEN TJ_Contract_Amount = 0 THEN 0 
+                ELSE TJ_Margin / TJ_Contract_Amount 
+            END AS TJ_Margin_Pct
+            ,CASE
+                WHEN TJ_Margin < 0 THEN TJ_Margin
+                ELSE JTD_Percent_Complete * TJ_Margin
+            END AS JTD_Margin
+        FROM
+            wip_history_2
+    ),
+    wip_history_4 AS (
+        SELECT
+            *
+            ,JTD_Margin + JTD_Cost AS JTD_Revenue
+        FROM
+            wip_history_3
+    ),
+    wip_history_5 AS (
+        SELECT
+            *
+            ,TJ_Contract_Amount - JTD_Revenue AS Backlog_Revenue
+            ,TJ_Margin - JTD_Margin AS Backlog_Margin
+        FROM
+            wip_history_4
     )
-SELECT 
-    *
-FROM 
-    wip_calcs_4
-WHERE
-    job LIKE '1%'
-    AND
-    LEN(job) >= 7
-    AND
-    max_month >= DATEADD(month, -6, GETDATE())
-ORDER BY
+SELECT
     jcco
     ,job
     ,mth
+    ,TJ_Contract_Amount
+    ,TJ_Cost
+    ,TJ_Margin
+    ,TJ_Margin_Pct
+    ,JTD_Revenue
+    ,JTD_Cost
+    ,JTD_Margin
+    ,JTD_Percent_Complete
+    ,Backlog_Revenue
+    ,Backlog_Margin
+FROM 
+    wip_history_5
