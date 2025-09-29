d50a38122bad021d79e2faeb386d41767959fa55
Dan Morgan <dmorgan@shookconstruction.com>
2025-09-29 19:45:33 +0000

    Added file_month_year to rolling forecast staging

--- models/staging/sharepoint/rolling_forecast_summary.sql
+++ models/staging/sharepoint/rolling_forecast_summary.sql
@@ -5,8 +5,13 @@ with rf as (
         "category" as category,
         "revenue" as revenue,
         "margin" as margin,
-        "pct_margin" as pct_margin
+        "pct_margin" as pct_margin,
+        md.file_month_year as file_month_year
     from {{ get_active_rf_table() }}
+    cross join 
+    {{ source('metadata', 'rolling_forecast_file_control') }} as md
+    where md.active = True
+
 )
 
 select * from rf
