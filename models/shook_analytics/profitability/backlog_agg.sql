6a3adf21a6971b1e585e55c58260ee2f0669d026
Dan Morgan <dmorgan@shookconstruction.com>
2025-10-15 15:20:40 +0000

    Integrate pending backlog to backlog aggregation

--- models/shook_analytics/profitability/backlog_agg.sql
+++ models/shook_analytics/profitability/backlog_agg.sql
@@ -4,21 +4,34 @@ with backlog as (
     from {{ ref('project_backlog') }}
 ), backlog_agg as (
     select 
-        cast(file_month_year as date) as file_month_year,
+        cast(file_month_year as date) as mth,
         sum(revenue_backlog) * 1000 as revenue_backlog,
         sum(ytd_revenue) as ytd_revenue
     from 
         backlog
-    group by file_month_year
+    group by 
+        file_month_year
+), pending_backlog_agg as (
+    select
+        mth,
+        sum(pending_backlog_revenue) as revenue_pending_backlog
+    from 
+        {{ ref('pending_backlog_prof') }}
+    group by
+        mth
 ), backlog_calcs as (
     select
-        file_month_year,
-        month(file_month_year) as mth_of_year,
-        revenue_backlog,
-        ytd_revenue,
-        revenue_backlog / ( ytd_revenue / month(file_month_year) ) as backlog_months
+        ba.mth,
+        month(ba.mth) as mth_of_year,
+        ba.revenue_backlog,
+        pb.revenue_pending_backlog,
+        ba.ytd_revenue,
+        ba.revenue_backlog / ( ba.ytd_revenue / month(ba.mth) ) as executed_backlog_months,
+        pb.revenue_pending_backlog/ ( ba.ytd_revenue / month(ba.mth) ) as pending_backlog_months
     from 
-        backlog_agg
+        backlog_agg as ba
+        left join pending_backlog_agg as pb
+            on ba.mth = pb.mth
 )
 
 select * from backlog_calcs
--- models/shook_analytics/profitability/pending_backlog_prof.sql
+++ models/shook_analytics/profitability/pending_backlog_prof.sql
@@ -1,6 +1,11 @@
 with pending_backlog as (
     select 
-        *
+        to_date(month_date) as mth,
+        region as region_name,
+        market_channel as market_name,
+        project,
+        cast(pending_backlog_revenue as float) as pending_backlog_revenue,
+        cast(pending_backlog_margin as float) as pending_backlog_margin
     from 
         {{ ref('pending_backlog') }}
 )
