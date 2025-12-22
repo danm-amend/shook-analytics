90f7e458284930d870fe4f8ca4adda8f693bde71
Dan Morgan <dmorgan@shookconstruction.com>
2025-12-22 19:14:48 +0000

    altered CTE names in wip_union

--- models/staging/sharepoint/wip_union.sql
+++ models/staging/sharepoint/wip_union.sql
@@ -1,7 +1,7 @@
-with union_sales as (
+with union_wip as (
     {{ union_tables_dynamic(source('metadata', 'wip_file_control'), 'shookdw', 'sharepoint') }}
 )
 
 select 
     *
-from union_sales
+from union_wip
