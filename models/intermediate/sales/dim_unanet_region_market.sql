5cadce458309a4b12d9e53ccc9158f7e48b90e83
Dan Morgan <dmorgan@shookconstruction.com>
2026-01-06 21:02:11 +0000

    fix: adjsuted reference column in unanet_region_market

--- models/intermediate/sales/dim_unanet_region_market.sql
+++ models/intermediate/sales/dim_unanet_region_market.sql
@@ -1,7 +1,7 @@
 with region_market as (
     select distinct 
     office_division_id, office_division_description,
-    PracticeAreaId as practice_area_id, PracticeAreaName as practice_area_name
+    practice_area_id, practice_area_name
     from {{ ref('stg_office_division') }}
     cross join 
     {{ ref('stg_practice_areas') }}
